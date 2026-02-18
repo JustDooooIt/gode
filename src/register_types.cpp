0b8d5e51a55ef4f01a4a179c91b666c15a499944
JustDooooIt <844730658@qq.com>
2026-02-18 18:12:12 +0800

    添加helloworld

---
+++ src/register_types.cpp
@@ -0,0 +1,34 @@
+#include "register_types.h"
+#include "hello_world.h"
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
+
+using namespace godot;
+
+void initialize_hello_world_module(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
+
+    ClassDB::register_class<HelloWorld>();
+}
+
+void uninitialize_hello_world_module(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
+}
+
+extern "C" {
+// Initialization.
+GDExtensionBool GDE_EXPORT hello_world_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+    godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+
+    init_obj.register_initializer(initialize_hello_world_module);
+    init_obj.register_terminator(uninitialize_hello_world_module);
+    init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+
+    return init_obj.init();
+}
+}
