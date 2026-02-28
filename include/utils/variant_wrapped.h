b639d7a90780f09c5bcd321698d5b28b91d0b524
JustDooooIt <844730658@qq.com>
2026-02-28 18:42:16 +0800

    完成基本绑定

---
+++ include/utils/variant_wrapped.h
@@ -0,0 +1,11 @@
+//
+// Created by 84473 on 2026/2/27.
+//
+
+#ifndef GODE_VARIANTWRAPPED_H
+#define GODE_VARIANTWRAPPED_H
+
+class VariantWrapped {
+};
+
+#endif //GODE_VARIANTWRAPPED_H
---
+++ src/utils/variant_wrapped.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 84473 on 2026/2/27.
+//
+
+#include "VariantWrapped.h"
