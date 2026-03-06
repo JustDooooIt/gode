e655cdaaacc41ab238840d9ae7f818d9d8eca74b
JustDooooIt <844730658@qq.com>
2026-03-06 17:11:58 +0800

    refactor: 统一调用内置方法的函数接口
    - 将多个专用的 `call_builtin_*_method_*` 函数合并为统一的 `call_builtin_method` 模板函数
    - 简化模板文件中的方法调用逻辑，移除对返回值和方法类型的条件分支
    - 重构 `func_utils.h`，通过函数重载和模板特化处理静态方法、实例方法、常方法和变参方法
    - 更新所有相关的 Jinja2 模板以使用新的统一接口

--- include/utils/func_utils.h
+++ include/utils/func_utils.h
@@ -28,7 +28,6 @@ inline T convert_arg(const godot::Variant &v) {
 	}
 }
 
-// 把 Napi::CallbackInfo 转换为 Napi::Array
 inline std::vector<Napi::Value> to_args_array(const Napi::CallbackInfo &info) {
 	std::size_t argc = info.Length();
 	std::vector<Napi::Value> args;
@@ -38,29 +37,49 @@ inline std::vector<Napi::Value> to_args_array(const Napi::CallbackInfo &info) {
 	return args;
 }
 
-// 调用 C++ 静态方法
-template <typename... P, std::size_t... Is>
-inline Napi::Value call_builtin_static_method_no_ret_impl(void (*Func)(P...), Napi::Env env, std::vector<Napi::Value> args, std::index_sequence<Is...>) {
-	Func(napi_to_godot<P>(args[Is])...);
-	return env.Undefined();
-}
-template <typename... P>
-inline Napi::Value call_builtin_static_method_no_ret(void (*Func)(P...), const Napi::CallbackInfo &info) {
-	std::vector<Napi::Value> args = to_args_array(info);
-	return call_builtin_static_method_no_ret_impl(Func, info.Env(), args, std::make_index_sequence<sizeof...(P)>());
-}
-
+// Helper: Static Method Implementation
 template <typename R, typename... P, std::size_t... Is>
-inline Napi::Value call_builtin_static_method_ret_impl(R (*Func)(P...), Napi::Env env, std::vector<Napi::Value> args, std::index_sequence<Is...>) {
-	return godot_to_napi(env, Func(napi_to_godot<P>(args[Is])...));
-}
-template <typename R, typename... P>
-inline Napi::Value call_builtin_static_method_ret(R (*Func)(P...), const Napi::CallbackInfo &info) {
-	std::vector<Napi::Value> args = to_args_array(info);
-	return call_builtin_static_method_ret_impl(Func, info.Env(), args, std::make_index_sequence<sizeof...(P)>());
+inline Napi::Value call_builtin_method_impl(R (*Func)(P...), Napi::Env env, std::vector<Napi::Value> args, std::index_sequence<Is...>) {
+	if constexpr (std::is_void_v<R>) {
+		Func(napi_to_godot<P>(args[Is])...);
+		return env.Undefined();
+	} else {
+		return godot_to_napi(env, Func(napi_to_godot<P>(args[Is])...));
+	}
 }
 
-inline Napi::Value call_builtin_static_vararg_method_no_ret(void (*Func)(const godot::Variant **, GDExtensionInt), const Napi::CallbackInfo &info) {
+// Helper: Instance Method Implementation (Non-Const)
+template <typename T, typename R, typename... P, std::size_t... Is>
+inline Napi::Value call_builtin_method_impl(R (T::*Func)(P...), T *instance, Napi::Env env, std::vector<Napi::Value> args, std::index_sequence<Is...>) {
+	if constexpr (std::is_void_v<R>) {
+		(instance->*Func)(napi_to_godot<P>(args[Is])...);
+		return env.Undefined();
+	} else {
+		return godot_to_napi(env, (instance->*Func)(napi_to_godot<P>(args[Is])...));
+	}
+}
+
+// Helper: Instance Method Implementation (Const)
+template <typename T, typename R, typename... P, std::size_t... Is>
+inline Napi::Value call_builtin_method_impl(R (T::*Func)(P...) const, T *instance, Napi::Env env, std::vector<Napi::Value> args, std::index_sequence<Is...>) {
+	if constexpr (std::is_void_v<R>) {
+		(instance->*Func)(napi_to_godot<P>(args[Is])...);
+		return env.Undefined();
+	} else {
+		return godot_to_napi(env, (instance->*Func)(napi_to_godot<P>(args[Is])...));
+	}
+}
+
+// 1. Static Method (Regular)
+template <typename R, typename... P>
+inline Napi::Value call_builtin_method(R (*Func)(P...), const Napi::CallbackInfo &info) {
+	std::vector<Napi::Value> args = to_args_array(info);
+	return call_builtin_method_impl(Func, info.Env(), args, std::make_index_sequence<sizeof...(P)>());
+}
+
+// 2. Static Method (Vararg)
+// Matches specific signature: void (*)(const godot::Variant **, GDExtensionInt)
+inline Napi::Value call_builtin_method(void (*Func)(const godot::Variant **, GDExtensionInt), const Napi::CallbackInfo &info) {
 	std::vector<Napi::Value> args = to_args_array(info);
 	std::vector<godot::Variant> variant_args;
 	std::vector<godot::Variant *> arg_ptrs;
@@ -78,8 +97,11 @@ inline Napi::Value call_builtin_static_vararg_method_no_ret(void (*Func)(const g
 	Func((const godot::Variant **)arg_ptrs.data(), (GDExtensionInt)arg_ptrs.size());
 	return info.Env().Undefined();
 }
+
+// 3. Static Method (Vararg with Return)
+// Matches specific signature: R (*)(const godot::Variant **, GDExtensionInt)
 template <typename R>
-inline Napi::Value call_builtin_static_vararg_method_ret(R (*Func)(const godot::Variant **, GDExtensionInt), const Napi::CallbackInfo &info) {
+inline Napi::Value call_builtin_method(R (*Func)(const godot::Variant **, GDExtensionInt), const Napi::CallbackInfo &info) {
 	std::vector<Napi::Value> args = to_args_array(info);
 	std::vector<godot::Variant> variant_args;
 	std::vector<godot::Variant *> arg_ptrs;
@@ -97,27 +119,11 @@ inline Napi::Value call_builtin_static_vararg_method_ret(R (*Func)(const godot::
 	return godot_to_napi(info.Env(), Func((const godot::Variant **)arg_ptrs.data(), (GDExtensionInt)arg_ptrs.size()));
 }
 
-template <typename T, typename R>
-inline Napi::Value call_builtin_vararg_method_ret(R (*Func)(T *, const godot::Variant **, GDExtensionInt), T *instance, const Napi::CallbackInfo &info) {
-	std::vector<Napi::Value> args = to_args_array(info);
-	std::vector<godot::Variant> variant_args;
-	std::vector<godot::Variant *> arg_ptrs;
 
-	variant_args.reserve(args.size());
-	arg_ptrs.reserve(args.size());
-
-	for (const auto &arg : args) {
-		variant_args.push_back(napi_to_godot(arg));
-	}
-	for (size_t i = 0; i < variant_args.size(); i++) {
-		arg_ptrs.push_back(&variant_args[i]);
-	}
-
-	return godot_to_napi(info.Env(), Func(instance, (const godot::Variant **)arg_ptrs.data(), (GDExtensionInt)arg_ptrs.size()));
-}
-
+// 4. Instance Method (Vararg Helper - Static function taking T*)
+// Matches specific signature: void (*)(T *, const godot::Variant **, GDExtensionInt)
 template <typename T>
-inline Napi::Value call_builtin_vararg_method_no_ret(void (*Func)(T *, const godot::Variant **, GDExtensionInt), T *instance, const Napi::CallbackInfo &info) {
+inline Napi::Value call_builtin_method(void (*Func)(T *, const godot::Variant **, GDExtensionInt), T *instance, const Napi::CallbackInfo &info) {
 	std::vector<Napi::Value> args = to_args_array(info);
 	std::vector<godot::Variant> variant_args;
 	std::vector<godot::Variant *> arg_ptrs;
@@ -135,168 +141,49 @@ inline Napi::Value call_builtin_vararg_method_no_ret(void (*Func)(T *, const god
 	Func(instance, (const godot::Variant **)arg_ptrs.data(), (GDExtensionInt)arg_ptrs.size());
 	return info.Env().Undefined();
 }
-//
-// template <typename T, typename... P, std::size_t... Is>
-// inline Napi::Value call_builtin_vararg_method_no_ret_impl(void (T::*Func)(const godot::Variant &, P...), T *instance, Napi::Env env, std::vector<Napi::Value> args, std::index_sequence<Is...>) {
-// 	if (args.empty()) {
-// 		return env.Undefined();
-// 	}
-// 	godot::Variant first = napi_to_godot(args[0]);
-// 	(instance->*Func)(first, convert_arg<P>(napi_to_godot(args[Is + 1]))...);
-// 	return env.Undefined();
-// }
-// template <typename T, typename A1, typename... P>
-// inline Napi::Value call_builtin_vararg_method_no_ret(void (T::*Func)(A1, P...), T *instance, const Napi::CallbackInfo &info) {
-// 	std::vector<Napi::Value> args = to_args_array(info);
-// 	return call_builtin_vararg_method_no_ret_impl(Func, instance, info.Env(), args, std::make_index_sequence<sizeof...(P)>());
-// }
-//
-// template <typename T, typename... P>
-// inline Napi::Value call_builtin_vararg_method_no_ret(void (T::*Func)(P...), T *instance, const Napi::CallbackInfo &info) {
-// 	std::vector<Napi::Value> args = to_args_array(info);
-// 	return call_builtin_vararg_method_no_ret_impl(Func, instance, info.Env(), args, std::make_index_sequence<sizeof...(P)>());
-// }
-//
-// template <typename T, typename R, typename A1, typename... P, std::size_t... Is>
-// inline Napi::Value call_builtin_vararg_method_ret_impl(R (T::*Func)(A1, P...), T *instance, Napi::Env env, std::vector<Napi::Value> args, std::index_sequence<Is...>) {
-// 	if (args.empty()) {
-// 		return env.Undefined();
-// 	}
-// 	godot::Variant first = napi_to_godot(args[0]);
-// 	return godot_to_napi(env, (instance->*Func)(first, convert_arg<P>(napi_to_godot(args[Is + 1]))...));
-// }
-// template <typename T, typename R, typename A1, typename... P>
-// inline Napi::Value call_builtin_vararg_method_ret(R (T::*Func)(A1, P...), T *instance, const Napi::CallbackInfo &info) {
-// 	std::vector<Napi::Value> args = to_args_array(info);
-// 	return call_builtin_vararg_method_ret_impl(Func, instance, info.Env(), args, std::make_index_sequence<sizeof...(P)>());
-// }
-//
-// template <typename T, typename R, typename... P, std::size_t... Is>
-// inline Napi::Value call_builtin_vararg_method_ret_impl(R (T::*Func)(P...), T *instance, Napi::Env env, std::vector<Napi::Value> args, std::index_sequence<Is...>) {
-// 	return godot_to_napi(env, (instance->*Func)(napi_to_godot<P>(args[Is])...));
-// }
-// template <typename T, typename R, typename... P>
-// inline Napi::Value call_builtin_vararg_method_ret(R (T::*Func)(P...), T *instance, const Napi::CallbackInfo &info) {
-// 	std::vector<Napi::Value> args = to_args_array(info);
-// 	return call_builtin_vararg_method_ret_impl(Func, instance, info.Env(), args, std::make_index_sequence<sizeof...(P)>());
-// }
-//
-// template <typename T, typename A1, typename... P, std::size_t... Is>
-// inline Napi::Value call_builtin_const_vararg_method_no_ret_impl(void (T::*Func)(A1, P...) const, T *instance, Napi::Env env, std::vector<Napi::Value> args, std::index_sequence<Is...>) {
-// 	if (args.empty()) {
-// 		return env.Undefined();
-// 	}
-// 	godot::Variant first = napi_to_godot(args[0]);
-// 	(instance->*Func)(first, convert_arg<P>(napi_to_godot(args[Is + 1]))...);
-// 	return env.Undefined();
-// }
-// template <typename T, typename A1, typename... P>
-// inline Napi::Value call_builtin_const_vararg_method_no_ret(void (T::*Func)(A1, P...) const, T *instance, const Napi::CallbackInfo &info) {
-// 	std::vector<Napi::Value> args = to_args_array(info);
-// 	return call_builtin_const_vararg_method_no_ret_impl(Func, instance, info.Env(), args, std::make_index_sequence<sizeof...(P)>());
-// }
-//
-// template <typename T, typename... P, std::size_t... Is>
-// inline Napi::Value call_builtin_const_vararg_method_no_ret_impl(void (T::*Func)(P...) const, T *instance, Napi::Env env, std::vector<Napi::Value> args, std::index_sequence<Is...>) {
-// 	(instance->*Func)(napi_to_godot<P>(args[Is])...);
-// 	return env.Undefined();
-// }
-// template <typename T, typename... P>
-// inline Napi::Value call_builtin_const_vararg_method_no_ret(void (T::*Func)(P...) const, T *instance, const Napi::CallbackInfo &info) {
-// 	std::vector<Napi::Value> args = to_args_array(info);
-// 	return call_builtin_const_vararg_method_no_ret_impl(Func, instance, info.Env(), args, std::make_index_sequence<sizeof...(P)>());
-// }
-//
-// template <typename T, typename R, typename... P, std::size_t... Is>
-// inline Napi::Value call_builtin_const_vararg_method_ret_impl(R (T::*Func)(P...) const, T *instance, Napi::Env env, std::vector<Napi::Value> args, std::index_sequence<Is...>) {
-// 	return godot_to_napi(env, (instance->*Func)(napi_to_godot<P>(args[Is])...));
-// }
-// template <typename T, typename R, typename... P>
-// inline Napi::Value call_builtin_const_vararg_method_ret(R (T::*Func)(P...) const, T *instance, const Napi::CallbackInfo &info) {
-// 	std::vector<Napi::Value> args = to_args_array(info);
-// 	return call_builtin_const_vararg_method_ret_impl(Func, instance, info.Env(), args, std::make_index_sequence<sizeof...(P)>());
-// }
-//
-// template <typename T, typename R, typename A1, typename... P, std::size_t... Is>
-// inline Napi::Value call_builtin_const_vararg_method_ret_impl(R (T::*Func)(A1, P...) const, T *instance, Napi::Env env, std::vector<Napi::Value> args, std::index_sequence<Is...>) {
-// 	if (args.empty()) {
-// 		return env.Undefined();
-// 	}
-// 	godot::Variant first = napi_to_godot(args[0]);
-// 	return godot_to_napi(env, (instance->*Func)(first, convert_arg<P>(napi_to_godot(args[Is + 1]))...));
-// }
-// template <typename T, typename R, typename A1, typename... P>
-// inline Napi::Value call_builtin_const_vararg_method_ret(R (T::*Func)(A1, P...) const, T *instance, const Napi::CallbackInfo &info) {
-// 	std::vector<Napi::Value> args = to_args_array(info);
-// 	return call_builtin_const_vararg_method_ret_impl(Func, instance, info.Env(), args, std::make_index_sequence<sizeof...(P)>());
-// }
 
-template <typename T, typename... P, std::size_t... Is>
-inline Napi::Value call_builtin_const_method_no_ret_impl(void (T::*Func)(P...) const, T *instance, Napi::Env env, std::vector<Napi::Value> args, std::index_sequence<Is...>) {
-	(instance->*Func)(napi_to_godot<P>(args[Is])...);
-	return env.Undefined();
-}
-template <typename T, typename... P>
-inline Napi::Value call_builtin_const_method_no_ret(void (T::*Func)(P...) const, T *instance, const Napi::CallbackInfo &info) {
+// 5. Instance Method (Vararg Helper with Return - Static function taking T*)
+// Matches specific signature: R (*)(T *, const godot::Variant **, GDExtensionInt)
+template <typename T, typename R>
+inline Napi::Value call_builtin_method(R (*Func)(T *, const godot::Variant **, GDExtensionInt), T *instance, const Napi::CallbackInfo &info) {
 	std::vector<Napi::Value> args = to_args_array(info);
-	return call_builtin_const_method_no_ret_impl(Func, instance, info.Env(), args, std::make_index_sequence<sizeof...(P)>());
-}
-template <typename T, typename... P, std::size_t... Is>
-inline Napi::Value call_builtin_method_no_ret_impl(void (T::*Func)(P...), T *instance, Napi::Env env, std::vector<Napi::Value> args, std::index_sequence<Is...>) {
-	(instance->*Func)(napi_to_godot<P>(args[Is])...);
-	return env.Undefined();
-}
-template <typename T, typename... P>
-inline Napi::Value call_builtin_method_no_ret(void (T::*Func)(P...), T *instance, const Napi::CallbackInfo &info) {
-	std::vector<Napi::Value> args = to_args_array(info);
-	return call_builtin_method_no_ret_impl(Func, instance, info.Env(), args, std::make_index_sequence<sizeof...(P)>());
+	std::vector<godot::Variant> variant_args;
+	std::vector<godot::Variant *> arg_ptrs;
+
+	variant_args.reserve(args.size());
+	arg_ptrs.reserve(args.size());
+
+	for (const auto &arg : args) {
+		variant_args.push_back(napi_to_godot(arg));
+	}
+	for (size_t i = 0; i < variant_args.size(); i++) {
+		arg_ptrs.push_back(&variant_args[i]);
+	}
+
+	return godot_to_napi(info.Env(), Func(instance, (const godot::Variant **)arg_ptrs.data(), (GDExtensionInt)arg_ptrs.size()));
 }
 
+
+// 6. Instance Method (Regular Non-Const)
+template <typename T, typename R, typename... P>
+inline Napi::Value call_builtin_method(R (T::*Func)(P...), T *instance, const Napi::CallbackInfo &info) {
+	std::vector<Napi::Value> args = to_args_array(info);
+	return call_builtin_method_impl(Func, instance, info.Env(), args, std::make_index_sequence<sizeof...(P)>());
+}
+
+// 7. Instance Method (Regular Const)
+template <typename T, typename R, typename... P>
+inline Napi::Value call_builtin_method(R (T::*Func)(P...) const, T *instance, const Napi::CallbackInfo &info) {
+	std::vector<Napi::Value> args = to_args_array(info);
+	return call_builtin_method_impl(Func, instance, info.Env(), args, std::make_index_sequence<sizeof...(P)>());
+}
+
+// 8. Setter (Regular Non-Const)
 template <typename T, typename... P>
-inline Napi::Value call_builtin_method_no_ret(void (T::*Func)(P...), T *instance, const Napi::CallbackInfo &info, const Napi::Value &val) {
+inline Napi::Value call_builtin_method(void (T::*Func)(P...), T *instance, const Napi::CallbackInfo &info, const Napi::Value &val) {
 	(instance->*Func)(napi_to_godot<P>(val)...);
 	return info.Env().Undefined();
 }
 
-template <typename T, typename R, typename... P, std::size_t... Is>
-inline Napi::Value call_builtin_method_ret_impl(R (T::*Func)(P...), T *instance, Napi::Env env, std::vector<Napi::Value> args, std::index_sequence<Is...>) {
-	return godot_to_napi(env, (instance->*Func)(napi_to_godot<P>(args[Is])...));
-}
-template <typename T, typename R, typename... P>
-inline Napi::Value call_builtin_method_ret(R (T::*Func)(P...), T *instance, const Napi::CallbackInfo &info) {
-	std::vector<Napi::Value> args = to_args_array(info);
-	return call_builtin_method_ret_impl(Func, instance, info.Env(), args, std::make_index_sequence<sizeof...(P)>());
-}
-
-template <typename T, typename R, typename... P, std::size_t... Is>
-inline Napi::Value call_builtin_const_method_ret_impl(R (T::*Func)(P...) const, T *instance, Napi::Env env, std::vector<Napi::Value> args, std::index_sequence<Is...>) {
-	return godot_to_napi(env, (instance->*Func)(napi_to_godot<P>(args[Is])...));
-}
-template <typename T, typename R, typename... P>
-inline Napi::Value call_builtin_const_method_ret(R (T::*Func)(P...) const, T *instance, const Napi::CallbackInfo &info) {
-	std::vector<Napi::Value> args = to_args_array(info);
-	return call_builtin_const_method_ret_impl(Func, instance, info.Env(), args, std::make_index_sequence<sizeof...(P)>());
-}
-
-// Specializations for methods with 0 arguments to fix deduction issues
-template <typename T>
-inline Napi::Value call_builtin_const_method_no_ret(void (T::*Func)() const, T *instance, const Napi::CallbackInfo &info) {
-	(instance->*Func)();
-	return info.Env().Undefined();
-}
-template <typename T>
-inline Napi::Value call_builtin_method_no_ret(void (T::*Func)(), T *instance, const Napi::CallbackInfo &info) {
-	(instance->*Func)();
-	return info.Env().Undefined();
-}
-
-template <typename T, typename R>
-inline Napi::Value call_builtin_const_method_ret(R (T::*Func)() const, T *instance, const Napi::CallbackInfo &info) {
-	return godot_to_napi(info.Env(), (instance->*Func)());
-}
-template <typename T, typename R>
-inline Napi::Value call_builtin_method_ret(R (T::*Func)(), T *instance, const Napi::CallbackInfo &info) {
-	return godot_to_napi(info.Env(), (instance->*Func)());
-}
 } //namespace gode
 #endif // GODE_FUNC_UTILS_H
